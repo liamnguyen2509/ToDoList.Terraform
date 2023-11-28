376829b30075cbe472bc7505e981fea6d0aab3f1
Liam Nguyen <lam.nguyenthai2509@gmail.com>
2023-11-28 23:58:48 +0700

    config cloud

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "3.75.0"
     }
   }
+
+  cloud {
+    organization = "liamnguyen2509"
+    workspaces {
+      name = "ToDoListTerraform"
+    }
+  }
 }
 
 module "resource-group" {
@@ -15,32 +22,32 @@ module "resource-group" {
 }
 
 module "database" {
-  source                                       = "./modules/database"
-  depends_on                                   = [module.resource-group]
-  resource_group_name                          = var.resource_group_name
-  location                                     = var.location
+  source              = "./modules/database"
+  depends_on          = [module.resource-group]
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "key-vault" {
-  source                                       = "./modules/key-vaults"
-  depends_on                                   = [module.resource-group]
-  resource_group_name                          = var.resource_group_name
-  location                                     = var.location
-  tenant_id                                    = var.tenant_id
-  object_id                                    = var.object_id
-  key_vault_name                               = var.key_vault_name
-  secret_name                                  = var.secret_name
-  secret_value                                 = var.secret_value
+  source              = "./modules/key-vaults"
+  depends_on          = [module.resource-group]
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tenant_id           = var.tenant_id
+  object_id           = var.object_id
+  key_vault_name      = var.key_vault_name
+  secret_name         = var.secret_name
+  secret_value        = var.secret_value
 }
 
 module "app" {
-  source                                       = "./modules/app"
-  depends_on                                   = [module.resource-group, module.database, module.key-vault]
-  resource_group_name                          = var.resource_group_name
-  location                                     = var.location
-  service_plan_name                            = var.service_plan_name
-  os_type                                      = var.os_type
-  sku_name                                     = var.sku_name
+  source              = "./modules/app"
+  depends_on          = [module.resource-group, module.database, module.key-vault]
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_name   = var.service_plan_name
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
 # Configure the Microsoft Azure Provider
